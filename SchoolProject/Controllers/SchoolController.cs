3600d21be862a106d0e9e5d17278e5387977ed33
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-18 10:31:00 +0100

    számításos feladatok

--- SchoolProject/Controllers/SchoolController.cs
+++ SchoolProject/Controllers/SchoolController.cs
@@ -11,36 +11,10 @@ namespace SchoolProject.Controllers
     {
         private readonly SchoolContext _context = new SchoolContext();
 
-        [HttpGet("Students")]
+        [HttpGet("students")]
         public async Task<IActionResult> GetAllStudentsAsync()
         {
             return Ok(_context.Students);
-        }
-
-        [HttpGet("NumberOfStudents")]
-        public async Task<IActionResult> GetNumberOfStudentsAsync()
-        {
-            return Ok(_context.Students.Count());
-        }
-
-        [HttpGet("Departments")]
-        public async Task<IActionResult> GetAllDepartmentsAsync()
-        {
-            return Ok(_context.Departments);
-        }
-
-        [HttpGet("StudentsOfDepartment")]
-        public async Task<IActionResult> GetStudentsOfDepartmentAsync([FromQuery] string departmentName)
-        {
-            var result = await (from s in _context.Students
-                             from d in _context.Departments
-                             where d.Id == s.DepartmentId && d.Name == departmentName
-                             select s.Name).ToListAsync();
-
-            return Ok(result);
-            /*_context.Departments.FirstOrDefault(d => d.Name == departmentName);
-        return Ok(_context.Departments)*/
-            ;
         }
 
         [HttpGet("Courses")]
@@ -54,5 +28,50 @@ namespace SchoolProject.Controllers
         {
             return Ok(_context.Teachers);
         }
+
+
+        [HttpGet("Departments")]
+        public async Task<IActionResult> GetAllDepartmentsAsync()
+        {
+            return Ok(_context.Departments);
+        }
+
+        [HttpGet("StudentsOfDepartment")]
+        public async Task<IActionResult> GetStudentsOfDepartmentAsync([FromQuery] string departmentName)
+        {
+            var result = await (from s in _context.Students
+                                from d in _context.Departments
+                                where d.Id == s.DepartmentId && d.Name == departmentName
+                                select s.Name).ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("students/count")]
+        public async Task<IActionResult> GetNumberOfStudentsAsync()
+        {
+            return Ok(_context.Students.Count());
+        }
+
+        [HttpGet("courses/{course_id}/students/count")]
+        public async Task<IActionResult> GetNumberOfStudentsInACourseAsync(int course_id)
+        {
+            var course = await _context.Courses.FirstOrDefaultAsync(c => c.Id == course_id);
+            var department = await _context.Departments.FirstOrDefaultAsync(d => d.Id == course.DepartmentId);
+            return Ok(_context.Students.Where(s => s.DepartmentId == department.Id).Count());
+        }
+
+        [HttpGet("teachers/count")]
+        public async Task<IActionResult> GetNumberOfTeachersAsync()
+        {
+            return Ok(_context.Teachers.Count());
+        }
+
+        [HttpGet("departments/{department_id}/students/count")]
+        public async Task<IActionResult> GetNumberOfStudentsInADepartmentAsync(int department_id)
+        {
+            return Ok(_context.Students.Where(s => s.DepartmentId == department_id).Count());
+        }
+
     }
 }
